335ef909172b53659686d8e680467af19daafaaa
AstroKD <kdreyerinseattle@gmail.com>
2020-01-21 15:02:33 -0800

    material

--- webGLearth/app.js
+++ webGLearth/app.js
@@ -4,7 +4,7 @@ var scene = new THREE.Scene();
 
 // Create a new Perspective Camera
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.z = 5;
+camera.position.z = 1;
 
 // Create a Full Screen WebGL Renderer
 var renderer = new THREE.WebGLRenderer({antialias: true});
@@ -28,11 +28,15 @@ var geometry = new THREE.SphereGeometry(0.5, 32, 32);
 
 var material = new THREE.MeshPhongMaterial();
 
+// https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74218/world.200412.3x5400x2700.jpg
+// https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74092/world.200407.3x5400x2700.jpg
+
 var mesh = new THREE.Mesh(geometry, material);
 mesh.position.x = 0;
 mesh.position.y = 0;
 mesh.position.z = 0;
 scene.add(mesh);
+material.map = THREE.ImageUtils.loadTexture('images/earthmap1k.jpg');
 
 var light = new THREE.PointLight(0xFFFFFF, 2, 1000);
 light.position.set(0,2,2);
